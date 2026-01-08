e949991bd4387d8e072df784e8426ca461f8c01b
MrSinaf <mrsinaf@sinafproduction.xyz>
2026-01-08 17:43:41 +0100

    ( ^_^)／ Adding an example project for Ratelite
    > Ajout d'un exemple `hello_ratelite.rs` pour démontrer l'utilisation de la bibliothèque Ratelite.
    > Inclusion d'une image d'exemple `ratelite.png` dans les assets.

---
+++ examples/hello_ratelite.rs
@@ -0,0 +1,51 @@
+﻿use ratelite::ui::Canvas;
+use ratelite::ui::element::Widget;
+use ratelite::ui::widgets::Image;
+use ratelite::{GameContext, Ratelite, Scene, SceneAction, Shader, Texture2D, VecF2};
+
+fn main() {
+    Ratelite::launch(
+        "Hello Ratelite",
+        Home {
+            canvas: Canvas::new(),
+        },
+    );
+}
+
+struct Home {
+    canvas: Canvas,
+}
+
+impl Scene for Home {
+    fn start(&mut self, ctx: &mut GameContext) {
+        let logo_id = {
+            let asset = Texture2D::load(ctx.get_gl(), "examples/assets/ratelite.png");
+            ctx.get_vault_mut().add("ratelite", asset)
+        };
+        let shader_id = {
+            let asset = Shader::default(ctx.get_gl());
+            ctx.get_vault_mut().add("shader", asset)
+        };
+
+        self.canvas
+            .add_child(Image::new(ctx, shader_id, logo_id).with_element(|e| {
+                e.set_pivot_and_anchors(VecF2::new(0.5, 0.5));
+                e.set_scale(VecF2::new(10.0, 10.0));
+            }));
+
+        self.canvas.start(ctx);
+    }
+
+    fn update(&mut self, ctx: &mut GameContext) -> SceneAction {
+        self.canvas.update(ctx);
+        SceneAction::None
+    }
+
+    fn render(&self, ctx: &GameContext) {
+        self.canvas.render(ctx);
+    }
+
+    fn resized(&mut self, ctx: &mut GameContext) {
+        self.canvas.resize(ctx);
+    }
+}
