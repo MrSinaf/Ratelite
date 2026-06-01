a1a387adfa6645f3cf435a63715eded6e26bc9af
MrSinaf <mrsinaf@sinafproduction.xyz>
2026-06-01 10:21:15 +0200

    (｡･ω･｡) Add sexy `Toggle` and horny `ElementToggle` widget for interactive UI controls
    > Création d'une classe `Toggle` pour gérer les widgets interactifs avec des états alternés (`textTrue` et `textFalse`).
    > Introduction de `ElementToggle` comme classe de base pour gérer les interactions de clics et basculement de valeur.
    > Ajout d'une méthode `DefaultPrefab` pour définir automatiquement les propriétés visuelles et les événements.

---
+++ Ratelite.UI/Widgets/ElementToggle.cs
@@ -0,0 +1,58 @@
+﻿namespace Ratelite.UI.Widgets;
+
+public class ElementToggle : UIElement
+{
+	public readonly UIElement element;
+	public bool isPressed { get; private set; }
+	
+	public bool value
+	{
+		get;
+		set
+		{
+			field = value;
+			onToggle.Invoke(field);
+		}
+	}
+	
+	public event Action<bool> onToggle = delegate { };
+	public event Action<UIElement> onPressed = delegate { };
+	public event Action<UIElement> onReleased = delegate { };
+	
+	public ElementToggle(UIElement element,  Action<bool>? onToggle)
+	{
+		base.AddChild(this.element = element);
+		this.onToggle += onToggle;
+		
+		element.isInteractive = false;
+		R.game.window.mouseButtonPressed += OnMouseButtonPressed;
+		R.game.window.mouseButtonReleased += OnMouseButtonReleased;
+	}
+	
+	private void OnMouseButtonPressed(MouseButton button)
+	{
+		if (button == MouseButton.Left && isCursorOver)
+		{
+			isPressed = true;
+			onPressed(this);
+		}
+	}
+	
+	private void OnMouseButtonReleased(MouseButton button)
+	{
+		if (button == MouseButton.Left && isPressed)
+		{
+			isPressed = false;
+			onReleased(this);
+			
+			if (isCursorOver)
+				value = !value;
+		}
+	}
+	
+	public override void OnDestroy()
+	{
+		R.game.window.mouseButtonPressed -= OnMouseButtonPressed;
+		R.game.window.mouseButtonReleased -= OnMouseButtonReleased;
+	}
+}
---
+++ Ratelite.UI/Widgets/Toggle.cs
@@ -0,0 +1,61 @@
+﻿using Ratelite.Resources;
+
+namespace Ratelite.UI.Widgets;
+
+public class Toggle : ElementToggle
+{
+	public readonly Label label;
+	
+	public string textTrue
+	{
+		get;
+		set
+		{
+			field = value;
+			UpdateLabel();
+		}
+	}
+	public string textFalse
+	{
+		get;
+		set
+		{
+			field = value;
+			UpdateLabel();
+		}
+	}
+	
+	public Toggle(string textTrue, string textFalse, Action<bool>? onToggle, string? prefab = "")
+			: base(new Label(textFalse), onToggle)
+	{
+		label = (Label)element;
+		this.onToggle += OnClick;
+		this.textTrue = textTrue;
+		this.textFalse = textFalse;
+		UIPrefab.Apply(prefab, this);
+	}
+	
+	private void OnClick(bool value)=> UpdateLabel();
+	private void UpdateLabel() => label.text = value ? textTrue : textFalse;
+	
+	[IsDefaultPrefab]
+	public static void DefaultPrefrab(Toggle e)
+	{
+		e.material = Vault.GetAsset<MaterialUI>(UIModule.DEFAULT_MATERIAL);
+		e.mesh = Vault.GetAsset<Mesh>(UIModule.DEFAULT_MESH);
+		e.size = new Vector2(200, 30);
+		e.tint = new Color(0x26354A);
+		e.cornerRadius = new Region(8);
+		
+		e.label.pivotAndAnchors = new Vector2(0.5F);
+		
+		e.cursorEnter += OnMouseEnter;
+		e.cursorExit += OnMouseExit;
+		
+		void OnMouseExit(UIElement e)
+			=> e.tint = new Color(0x26354A);
+		
+		void OnMouseEnter(UIElement e)
+			=> e.tint = new Color(0x1C2739);
+	}
+}
