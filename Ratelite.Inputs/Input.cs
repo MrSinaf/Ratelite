b90144a384d1e2b3e72cd080128a95b2187f104f
MrSinaf <mrsinaf@sinafproduction.xyz>
2026-09-03 11:25:24 +0200

    ヽ(´ー｀)ノ Add additional input event handlers in `Input`
    > Ajout des événements `charTyped`, `cursorMoved` et `scrolled` pour la classe `Input`.
    > Connexion des nouveaux événements aux délégués correspondants dans `Init`.
    > Implémentation des méthodes `OnCharTyped`, `OnCursorMoved` et `OnScrolled` pour gérer ces nouveaux événements.

--- Ratelite.Inputs/Input.cs
+++ Ratelite.Inputs/Input.cs
@@ -1,27 +1,33 @@
-﻿using System.Numerics;
-
-namespace Ratelite.Inputs;
+﻿namespace Ratelite.Inputs;
 
 public static class Input
 {
 	private static readonly HashSet<Key> keysHolded = [];
 	private static readonly HashSet<MouseButton> mouseButtonsHolded = [];
 	
+	public static event Action<char> charTyped = delegate {};
 	public static event Action<Key> keyPressed = delegate {};
 	public static event Action<Key> keyHolded = delegate {};
 	public static event Action<Key> keyReleased = delegate {};
+	
 	public static event Action<MouseButton> mouseButtonPressed = delegate {};
 	public static event Action<MouseButton> mouseButtonHolded = delegate {};
 	public static event Action<MouseButton> mouseButtonReleased = delegate {};
+	
+	public static event Action<Vector2> cursorMoved = delegate {};
+	public static event Action<Vector2Int> scrolled = delegate { };
 	
 	public static Vector2 cursorPosition => R.game.window.cursorPosition;
 	
 	internal static void Init()
 	{
+		R.game.window.charTyped += OnCharTyped;
 		R.game.window.keyPressed += OnKeyPressed;
 		R.game.window.keyReleased += OnKeyReleased;
 		R.game.window.mouseButtonPressed += OnMouseButtonPressed;
 		R.game.window.mouseButtonReleased += OnButtonReleased;
+		R.game.window.cursorMoved += OnCursorMoved;
+		R.game.window.scrolled += OnScrolled;
 	}
 	
 	internal static void Update()
@@ -32,6 +38,12 @@ public static class Input
 		foreach (var button in mouseButtonsHolded)
 			mouseButtonHolded.Invoke(button);
 	}
+	
+	private static void OnCharTyped(char c) => charTyped.Invoke(c);
+	
+	private static void OnCursorMoved(Vector2 delta) => cursorMoved.Invoke(delta);
+
+	private static void OnScrolled(Vector2Int delta) => scrolled.Invoke(delta);
 	
 	private static void OnMouseButtonPressed(MouseButton button)
 	{
